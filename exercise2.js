f21f982491601d4b6665e27c8947e5b1b6f36974
prabhu-raja <prabhuraja@gmail.com>
2020-05-20 06:58:48 -0400

    12 - Exercise2

---
+++ exercise2.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const debugEx1 = require('debug')('mongo:ex1');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+  .then(() => debugEx1('Connected to 🌱 Mongodb Exercises 🏋️‍♂️🏋️‍♂️🏋️‍♂️'))
+  .catch(err => debugEx1('Error in connecting 🍃', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now},
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+  return await Course
+    .find({isPublished: true, tags: {$in: ['frontend', 'backend']}})
+    .sort({price: 'desc'})
+    .select('name author',)
+    // .select({name:1, author:1})
+}
+
+async function run() {
+  const Courses = await getCourses();
+  debugEx1(Courses);
+}
+run();
