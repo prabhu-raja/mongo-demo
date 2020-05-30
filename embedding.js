293b5bcbda795a14f56c0ed1fb9327a9d012b12b
prabhu-raja <prabhuraja@gmail.com>
2020-05-30 12:43:16 -0400

    28 - Relational - Embedding - Update Directly

--- embedding.js
+++ embedding.js
@@ -28,9 +28,14 @@ async function createCourse(name, author) {
 }
 
 async function updateCourse(courseId, authorName) {
-  const course = await Course.findById(courseId);
-  course.author.name = authorName;
-  course.save();
+  const course = await Course.update(
+    { _id: courseId },
+    { 
+      $set: {
+        'author.name': authorName
+      }
+    });
+    console.log(JSON.stringify(course, null, 2));
 }
 
 async function listCourses() { 
@@ -39,4 +44,4 @@ async function listCourses() {
 }
 
 // createCourse('Node Course', new Author({ name: 'Mosh' }));
-// updateCourse('5ed28948e56ad91b540da9db', 'Mosh Hamedani 👨‍');
+updateCourse('5ed28948e56ad91b540da9db', 'John Smith 👨‍🦱');
